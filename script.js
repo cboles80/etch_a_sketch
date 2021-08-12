e77dba30521e8c94fb7d53fecc3a3d443760df63
Chris Boles <cboles80@gmail.com>
2021-08-12 08:17:21 -0700

    Added change grid function and started a form for selecting the grid area

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 console.log('Hello World');
-var declaration = document.styleSheets[0].cssRules[0].style;
-// const numCells = parseInt(declaration.getPropertyValue(eascell))**2;
-// console.log(numCells);
 
+let countColumns = getComputedStyle(document.documentElement())
+let countCells = 256;
+const eascells = document.querySelectorAll(".eascell");
 const elContainer = document.querySelector(".eascontainer");
-console.log(elContainer);
+const elCountColumns = document.querySelector("#count-columns");
+console.log(elCountColumns);
 
-//Creates 16 X 16 grid manually.
+//Creates grid manually.
 function createGrid() {
     let myHTML = '';
-    for(let i = 0;i < 256; i++){
+    for(let i = 0;i < countCells; i++){
         myHTML += '<div class="eascell"></div>'
     }
     elContainer.innerHTML=myHTML;
 }
-
-createGrid();
-
-//(CHANGE THIS CLASSNAME)
-const eascells = document.querySelectorAll(".eascell");
 
 //Adds event listener to add class, turning cell color to black
 eascells.forEach(cell => {
@@ -26,3 +22,12 @@ eascells.forEach(cell => {
         cell.classList.add("turnblack");
     });
 });
+
+elCountColumns.addEventListener('change', () => {
+    console.log('Changed');
+    countCells = elCountColumns.value**2;
+    createGrid();
+})
+
+createGrid();
+
