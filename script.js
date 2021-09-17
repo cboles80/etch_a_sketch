32be2312fb7b3c35875419ae9013673059098ac9
Chris Boles <cboles80@gmail.com>
2021-09-16 23:51:20 -0700

    Added function and eventlisteners

--- script.js
+++ script.js
@@ -1,33 +1,72 @@
 console.log('Hello World');
 
-let countColumns = getComputedStyle(document.documentElement())
-let countCells = 256;
-const eascells = document.querySelectorAll(".eascell");
+// Create a styles variable that holds the document styles.
+var styles = getComputedStyle(document.documentElement);
+
+
+let eascells = document.querySelectorAll(".eascell");
+console.log('eascells', eascells);
 const elContainer = document.querySelector(".eascontainer");
 const elCountColumns = document.querySelector("#count-columns");
 console.log(elCountColumns);
+let countCols = 16;
+countCols = parseInt(elCountColumns.value);
+let countCells = countCols**2;
+
+function createGridEventListener() {
+    eascells.forEach(cell => {
+        cell.addEventListener('mouseover', handleMouseOver);
+    });
+    }
 
 //Creates grid manually.
 function createGrid() {
+    console.log('top createGrid');
     let myHTML = '';
     for(let i = 0;i < countCells; i++){
-        myHTML += '<div class="eascell"></div>'
+        // myHTML += '<div class="eascell" id="' + '_' + i + '"></div>';
+        myHTML += `<div class="eascell" id="_${i}"></div>`;
     }
     elContainer.innerHTML=myHTML;
+    eascells = document.querySelectorAll(".eascell");
+console.log('eascells', eascells);
+    createGridEventListener();
+    console.log('bottom createGrid');
 }
 
-//Adds event listener to add class, turning cell color to black
-eascells.forEach(cell => {
-    cell.addEventListener('mouseover', () => {
-        cell.classList.add("turnblack");
-    });
-});
+function handleMouseOver(e) {
+    console.log('top handleMouseOver');
+    // console.log(e);
+    let myCell = document.getElementById(e.target.id);
+    myCell.classList.add("turnblack");
+}
+
+function handleSubmit(e){
+    console.log('top handleSubmit');
+    console.log('keyup-submit');
+    console.log(e);
+    // e.preventDefault();
+    // if (e.key === 'Enter'){
+    //     countCols = parseInt(elCountColumns.value);
+    //     document.documentElement.style.setProperty('--eascell', countCols);
+    //     countCells = countCols**2;
+    //     console.log(countCells);
+    //     createGrid();
+    // }
+}
+createGrid();
 
 elCountColumns.addEventListener('change', () => {
     console.log('Changed');
-    countCells = elCountColumns.value**2;
+    countCols = parseInt(elCountColumns.value);
+    console.log(typeof countCols);
+    document.documentElement.style.setProperty('--eascell', countCols);
+    countCells = countCols**2;
+    console.log(countCells);
     createGrid();
 })
 
-createGrid();
+// elCountColumns.addEventListener('keyup', handleSubmit);
+elCountColumns.addEventListener('submit', handleSubmit);
 
+console.log('bottom of file');
